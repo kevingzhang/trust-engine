5360a2ac6b47a724412f8a5ee95128b20b81066e
kevingzhang <kevin.zhang.canada@gmail.com>
2019-10-07 11:55:53 -0700

    tab space 2

--- docker-api/src/docker_api/client.rs
+++ docker-api/src/docker_api/client.rs
@@ -1,5 +1,5 @@
 pub mod docker_api {
-    tonic::include_proto!("docker_pb");
+	tonic::include_proto!("docker_pb");
 }
 use http::header::HeaderValue;
 use docker_api::{client::GetDockerClient, DockerInfoRequest, DockerImagesRequest, DockerImagesReply};
@@ -7,27 +7,27 @@ use tonic::transport::Channel;
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
-    let channel = Channel::from_static("http://[::1]:50051")
-        .intercept_headers(|headers| {
-            headers.insert(
-                "authorization",
-                HeaderValue::from_static("Leo security agent with some-secret-token"),
-            );
-        })
-        .channel();
-    let mut client = GetDockerClient::new(channel);
-    let request = tonic::Request::new(DockerInfoRequest {
-        name: "info".into(),
-    });
+	let channel = Channel::from_static("http://[::1]:50051")
+		.intercept_headers(|headers| {
+			headers.insert(
+				"authorization",
+				HeaderValue::from_static("Leo security agent with some-secret-token"),
+			);
+		})
+		.channel();
+	let mut client = GetDockerClient::new(channel);
+	let request = tonic::Request::new(DockerInfoRequest {
+		name: "info".into(),
+	});
 
-    let response = client.get_docker_info(request).await?;
+	let response = client.get_docker_info(request).await?;
 
-    println!("RESPONSE={}", response.into_inner().info);
-    let request = tonic::Request::new(DockerImagesRequest {
-        req: "images".into(),
-    });
-    let response = client.get_docker_images(request).await?;
+	println!("RESPONSE={}", response.into_inner().info);
+	let request = tonic::Request::new(DockerImagesRequest {
+		req: "images".into(),
+	});
+	let response = client.get_docker_images(request).await?;
 
-    println!("RESPONSE={:#?}", response.into_inner().images);
-    Ok(())
+	println!("RESPONSE={:#?}", response.into_inner().images);
+	Ok(())
 }
--- docker-api/src/docker_api/mod.rs
+++ docker-api/src/docker_api/mod.rs
@@ -6,12 +6,12 @@ use std::vec::Vec;
 
 
 pub mod docker_pb {
-    tonic::include_proto!("docker_pb");
+	tonic::include_proto!("docker_pb");
 }
 
 use docker_pb::{
-    server::{GetDocker},
-    DockerInfoReply, DockerInfoRequest, DockerImagesReply
+	server::{GetDocker},
+	DockerInfoReply, DockerInfoRequest, DockerImagesReply
 };
 
 #[derive(Default)]
@@ -21,107 +21,107 @@ pub struct DockerReqHandler {
 
 #[tonic::async_trait]
 impl GetDocker for DockerReqHandler {
-    async fn get_docker_info(
-        &self,
-        request: Request<DockerInfoRequest>,
-    ) -> Result<Response<DockerInfoReply>, Status> {
-        println!("Into inner of request: {:?}", request.into_inner());
+	async fn get_docker_info(
+		&self,
+		request: Request<DockerInfoRequest>,
+	) -> Result<Response<DockerInfoReply>, Status> {
+		println!("Into inner of request: {:?}", request.into_inner());
 
 
-        let info  = get_info();
+		let info  = get_info();
 
-        let reply = docker_pb::DockerInfoReply {
-            info: serde_json::to_string_pretty(&info).unwrap(),
-        };
-        Ok(Response::new(reply))
-    }
+		let reply = docker_pb::DockerInfoReply {
+			info: serde_json::to_string_pretty(&info).unwrap(),
+		};
+		Ok(Response::new(reply))
+	}
 
-    async fn get_docker_images(
-        &self,
-        request: Request<
-        docker_pb::DockerImagesRequest>,
-    ) -> Result<Response<DockerImagesReply>, Status> {
-        println!("Got a request: {:?}", request);
-        
-        let images = get_images();
+	async fn get_docker_images(
+		&self,
+		request: Request<
+		docker_pb::DockerImagesRequest>,
+	) -> Result<Response<DockerImagesReply>, Status> {
+		println!("Got a request: {:?}", request);
+		
+		let images = get_images();
 
-        let reply = docker_pb::DockerImagesReply {
-            images: images
-        };
-        Ok(Response::new(reply))
-    }
+		let reply = docker_pb::DockerImagesReply {
+			images: images
+		};
+		Ok(Response::new(reply))
+	}
 }
 
 fn get_info() -> serde_json::Value {
-    let socket = Socket::new("/var/run/docker.sock");
-    let mut docker = Docker::new(socket.clone());
-    let _info : serde_json::Value = docker.get_info().unwrap();
-    _info
+	let socket = Socket::new("/var/run/docker.sock");
+	let mut docker = Docker::new(socket.clone());
+	let _info : serde_json::Value = docker.get_info().unwrap();
+	_info
 }
 
 fn get_images() -> std::vec::Vec<docker_pb::Image>{
-    let socket = Socket::new("/var/run/docker.sock");
-    let mut docker = Docker::new(socket.clone());
-    let images_value = docker.get_images_value();
-    let arr_images: Vec<Image> = serde_json::from_value(images_value.unwrap()).unwrap();
-    let mut ret = std::vec::Vec::new();
-    for c in arr_images{
-        println!("c : {:#?}", c);
-        let image : docker_pb::Image = c.to_docker_pb_image();
-        println!("c convert to image: {:#?}", image);
-        ret.push(image);
-    }
+	let socket = Socket::new("/var/run/docker.sock");
+	let mut docker = Docker::new(socket.clone());
+	let images_value = docker.get_images_value();
+	let arr_images: Vec<Image> = serde_json::from_value(images_value.unwrap()).unwrap();
+	let mut ret = std::vec::Vec::new();
+	for c in arr_images{
+		println!("c : {:#?}", c);
+		let image : docker_pb::Image = c.to_docker_pb_image();
+		println!("c convert to image: {:#?}", image);
+		ret.push(image);
+	}
 
-    ret
+	ret
 }
 
 #[derive(Default, serde::Deserialize, Debug)]
 pub struct Image{
-    pub Id: String,
-    pub Created: u64,
-    pub ParentId: Option<String>,
-    pub RepoDigests: Option<Vec<String>>,
-    pub Size: u64,
-    pub VirtualSize: u64,
-    pub Labels: Option<std::collections::HashMap<String, String>>,
-    pub RepoTags: Option<Vec<String>>
+	pub Id: String,
+	pub Created: u64,
+	pub ParentId: Option<String>,
+	pub RepoDigests: Option<Vec<String>>,
+	pub Size: u64,
+	pub VirtualSize: u64,
+	pub Labels: Option<std::collections::HashMap<String, String>>,
+	pub RepoTags: Option<Vec<String>>
 }
 impl Image{
-    pub fn to_docker_pb_image(&self) ->docker_pb::Image {
-        docker_pb::Image{
-            id: self.Id.to_string(),
-            created: self.Created,
-            parent_id: self.ParentId.clone().unwrap(),
-            repo_digests: self.RepoDigests.clone().unwrap(),
-            size: self.Size,
-            virtual_size: self.VirtualSize,
-            labels: self.Labels.clone().unwrap_or(std::collections::HashMap::new()),
-            tags: self.RepoTags.clone().unwrap_or(Vec::new()),
-            
-        }
-    }
+	pub fn to_docker_pb_image(&self) ->docker_pb::Image {
+		docker_pb::Image{
+			id: self.Id.to_string(),
+			created: self.Created,
+			parent_id: self.ParentId.clone().unwrap(),
+			repo_digests: self.RepoDigests.clone().unwrap(),
+			size: self.Size,
+			virtual_size: self.VirtualSize,
+			labels: self.Labels.clone().unwrap_or(std::collections::HashMap::new()),
+			tags: self.RepoTags.clone().unwrap_or(Vec::new()),
+			
+		}
+	}
 }
 
 /******
  * 
  * 
  * pub struct Image {
-    #[prost(string, tag = "1")]
-    pub id: std::string::String,
-    #[prost(uint64, tag = "2")]
-    pub created: u64,
-    #[prost(string, tag = "3")]
-    pub parent_id: std::string::String,
-    #[prost(string, repeated, tag = "4")]
-    pub repo_digests: ::std::vec::Vec<std::string::String>,
-    #[prost(uint64, tag = "5")]
-    pub size: u64,
-    #[prost(uint64, tag = "6")]
-    pub virtual_size: u64,
-    #[prost(map = "string, string", tag = "7")]
-    pub labels: ::std::collections::HashMap<std::string::String, std::string::String>,
-    #[prost(string, repeated, tag = "8")]
-    pub tags: ::std::vec::Vec<std::string::String>,
+	#[prost(string, tag = "1")]
+	pub id: std::string::String,
+	#[prost(uint64, tag = "2")]
+	pub created: u64,
+	#[prost(string, tag = "3")]
+	pub parent_id: std::string::String,
+	#[prost(string, repeated, tag = "4")]
+	pub repo_digests: ::std::vec::Vec<std::string::String>,
+	#[prost(uint64, tag = "5")]
+	pub size: u64,
+	#[prost(uint64, tag = "6")]
+	pub virtual_size: u64,
+	#[prost(map = "string, string", tag = "7")]
+	pub labels: ::std::collections::HashMap<std::string::String, std::string::String>,
+	#[prost(string, repeated, tag = "8")]
+	pub tags: ::std::vec::Vec<std::string::String>,
 }
  */
 
