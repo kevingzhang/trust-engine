4120f33fba7f3891643dffc649adce465d4c2f5b
kevingzhang <kevin.zhang.canada@gmail.com>
2019-10-16 12:53:03 -0700

    add pingpong test

--- socketJsTest/src/client.js
+++ socketJsTest/src/client.js
@@ -18,7 +18,7 @@ var net = require('net'),
 var SHUTDOWN = false;
 
 // Our socket
-const SOCKETFILE = '/code/test.sock';
+const SOCKETFILE = process.env.SOCKETFILE || '/socket/node.sock';
 mode = "client";
 // For simplicity of demonstration, both ends in this one file
 
@@ -26,39 +26,41 @@ mode = "client";
 console.info('Loading interprocess communications test');
 console.info('  Mode: %s \n  Socket: %s \n  Process: %s',mode,SOCKETFILE,process.pid);
 
-if(mode === "client"){
+const tryConnect = (pid)=>{
     // Connect to server.
     console.log("Connecting to server.");
-    const tryConnect = ()=>{
-        client = net.createConnection(SOCKETFILE)
-        .on('connect', ()=>{
-            console.log("Connected.");
-        })
-        // Messages are buffers. use toString
-        .on('data', function(data) {
-            data = data.toString();
+    
+    const client = net.createConnection(SOCKETFILE)
+    .on('connect', ()=>{
+        console.log("Connected.");
+        interactivesAfterConnection(client);
+        loopPingMessage(client, pid);
+    })
+    // Messages are buffers. use toString
+    .on('data', function(data) {
+        data = data.toString();
 
-            if(data === '__boop'){
-                console.info('Server sent boop. Confirming our snoot is booped.');
-                client.write('__snootbooped');
-                return;
-            }
-            if(data === '__disconnect'){
-                console.log('Server disconnected.')
-                return cleanup();
-            }
+        if(data === '__boop'){
+            console.info('Server sent boop. Confirming our snoot is booped.');
+            client.write('__snootbooped');
+            return;
+        }
+        if(data === '__disconnect'){
+            console.log('Server disconnected.')
+            return cleanup(client);
+        }
 
-            // Generic message handler
-            console.info('Server:', data)
-        })
-        .on('error', function(data) {
-            console.error('Server not active. Will try again after 15 seconds');
-            setTimeout(tryConnect, 15000);
-        })
-    };
-    tryConnect();
+        // Generic message handler
+        console.info('Server:', data)
+    })
+    .on('error', function(data) {
+        console.error('Server not active. Will try again after 15 seconds');
+        setTimeout(tryConnect, 15000);
+    })
+}
 
-     // Handle input from stdin.
+const interactivesAfterConnection = (client)=>{
+         // Handle input from stdin.
     var inputbuffer = "";
     process.stdin.on("data", function (data) {
         inputbuffer += data;
@@ -66,18 +68,27 @@ if(mode === "client"){
             var line = inputbuffer.substring(0, inputbuffer.indexOf("\n"));
             inputbuffer = inputbuffer.substring(inputbuffer.indexOf("\n") + 1);
             // Let the client escape
-            if(line === 'exit'){ return cleanup(); }
-            if(line === 'quit'){ return cleanup(); }
+            if(line === 'exit'){ return cleanup(client); }
+            if(line === 'quit'){ return cleanup(client); }
             client.write(line);
         }
     });
 
-    function cleanup(){
-        if(!SHUTDOWN){ SHUTDOWN = true;
-            console.log('\n',"Terminating.",'\n');
-            client.end();
-            process.exit(0);
-        }
-    }
+
     process.on('SIGINT', cleanup);
+};
+function cleanup(client){
+    if(!SHUTDOWN){ SHUTDOWN = true;
+        console.log('\n',"Terminating.",'\n');
+        client.end();
+        process.exit(0);
+    }
 }
+const loopPingMessage = (client, pid)=>{
+    setTimeout(()=>{
+        client.write("Ping from " + pid);
+        loopPingMessage(client, pid);
+    }, 10000);
+}
+
+tryConnect(process.pid);
--- socketJsTest/src/server.js
+++ socketJsTest/src/server.js
@@ -7,77 +7,111 @@
 **   client>  MODE=client node ipc.example.js
 **
 */
+const net = require('net');
+const fs = require('fs');
+const connections = {};
 
-var net = require('net'),
-    fs = require('fs'),
-    connections = {},
-    server, client, mode
-    ;
 
 // prevent duplicate exit messages
 var SHUTDOWN = false;
 
 // Our socket
-const SOCKETFILE = '/code/test.sock';
-mode = "client";
-// For simplicity of demonstration, both ends in this one file
+const SOCKETFILE = process.env.SOCKETFILE || '/socket/node.sock';
 
-
+mode = 'server';
 console.info('Loading interprocess communications test');
-console.info('  Mode: %s \n  Socket: %s \n  Process: %s',mode,SOCKETFILE,process.pid);
+console.info(' Server \n  Socket: %s \n  Process: %s',mode,SOCKETFILE,process.pid);
 
-if(mode === "client"){
-    // Connect to server.
-    console.log("Connecting to server.");
-    const tryConnect = ()=>{
-        client = net.createConnection(SOCKETFILE)
-        .on('connect', ()=>{
-            console.log("Connected.");
-        })
+function createServer(socket){
+    console.log('Creating server.');
+    var server = net.createServer(function(stream) {
+        console.log('Connection acknowledged.');
+
+        // Store all connections so we can terminate them if the server closes.
+        // An object is better than an array for these.
+        var self = Date.now();
+        connections[self] = (stream);
+        stream.on('end', function() {
+            console.log('Client disconnected.');
+            delete connections[self];
+        });
+
         // Messages are buffers. use toString
-        .on('data', function(data) {
-            data = data.toString();
-
-            if(data === '__boop'){
-                console.info('Server sent boop. Confirming our snoot is booped.');
-                client.write('__snootbooped');
+        stream.on('data', function(msg) {
+            msg = msg.toString();
+            if(msg === '__snootbooped'){
+                console.log("Client's snoot confirmed booped.");
                 return;
             }
-            if(data === '__disconnect'){
-                console.log('Server disconnected.')
-                return cleanup();
+
+            console.log('Client:', msg);
+
+            if(msg === 'foo'){
+                stream.write('bar');
             }
 
-            // Generic message handler
-            console.info('Server:', data)
-        })
-        .on('error', function(data) {
-            console.error('Server not active. Will try again after 15 seconds');
-            setTimeout(tryConnect, 15000);
-        })
-    };
-    tryConnect();
+            if(msg === 'baz'){
+                stream.write('qux');
+            }
 
-     // Handle input from stdin.
-    var inputbuffer = "";
-    process.stdin.on("data", function (data) {
-        inputbuffer += data;
-        if (inputbuffer.indexOf("\n") !== -1) {
-            var line = inputbuffer.substring(0, inputbuffer.indexOf("\n"));
-            inputbuffer = inputbuffer.substring(inputbuffer.indexOf("\n") + 1);
-            // Let the client escape
-            if(line === 'exit'){ return cleanup(); }
-            if(line === 'quit'){ return cleanup(); }
-            client.write(line);
-        }
-    });
-
-    function cleanup(){
-        if(!SHUTDOWN){ SHUTDOWN = true;
-            console.log('\n',"Terminating.",'\n');
-            client.end();
-            process.exit(0);
-        }
-    }
-    process.on('SIGINT', cleanup);
+            if(msg === 'here come dat boi'){
+                stream.write('Kill yourself.');
+            }
+            if(msg.startsWith("Ping")){
+                stream.write("Pong response to " + msg);
+            }
+        });
+    })
+    .listen(socket)
+    .on('connection', function(socket){
+        console.log('Client connected.');
+        console.log('Sending boop.');
+        socket.write('__boop');
+        //console.log(Object.keys(socket));
+    })
+    ;
+    return server;
 }
+
+
+// check for failed cleanup
+console.log('Checking for leftover socket.');
+fs.stat(SOCKETFILE, function (err, stats) {
+    if (err) {
+        // start server
+        console.log('No leftover socket found.');
+        const server = createServer(SOCKETFILE); return;
+    }
+    else{
+        // remove file then start server
+        console.log('Removing leftover socket.')
+        fs.unlink(SOCKETFILE, function(err){
+            if(err){
+                console.log("fs.unlink(SOCKETFILE) error. This should never happen.");
+                console.error(err); 
+                process.exit(0);
+            }
+            const server = createServer(SOCKETFILE); return;
+        });  
+    }
+    
+});
+
+// close all connections when the user does CTRL-C
+function cleanup(){
+    if(!SHUTDOWN){ SHUTDOWN = true;
+        console.log('\n',"Terminating.",'\n');
+        if(Object.keys(connections).length){
+            let clients = Object.keys(connections);
+            while(clients.length){
+                let client = clients.pop();
+                connections[client].write('__disconnect');
+                connections[client].end(); 
+            }
+        }
+        server.close();
+        process.exit(0);
+    }
+}
+process.on('SIGINT', cleanup);
+
